e8f555c6ee28d8da88177e3d5907388f8d2d1b81
Emelyanov Konstantin <1924gg@gmail.com>
2020-10-08 19:04:17 +0300

    Initial Commit

---
+++ treacker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  treacker
+//
+//  Created by Константин Емельянов on 08.10.2020.
+//  Copyright © 2020 Константин Емельянов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
